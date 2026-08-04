4655ff18902c1787e709a299e4cd17692d2478ff
Mahir Siam <mahirsiam2004@users.noreply.github.com>
2026-08-04 22:30:19 +0600

    connected to mongodb

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 require('dotenv').config()
 const app = require('./src/app')
+const {connectDB} = require('./src/config/db')
 const port = process.env.PORT || 3000
 
 
+connectDB();
 
 app.listen(port,()=>{
     console.log(`server is running on ${port}`)
---
+++ src/config/db.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = process.env.MONGO_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function connectDB() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } catch (err) {
+    console.log("mongo error: ", err);
+  }
+}
+
+module.exports = { client, connectDB };
